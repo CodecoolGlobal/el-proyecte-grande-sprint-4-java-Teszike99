8fafa7441530819c3cb831fa1f7454618edcdcfc
Tesszike99 <teszidani02@gmail.com>
2022-03-22 11:35:48 +0100

    Feature - spring file set up

---
+++ BetSafe/src/main/java/com/example/BetSafe/BetSafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.BetSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetSafeApplication.class, args);
+	}
+
+}
---
+++ BetSafe/src/test/java/com/example/BetSafe/BetSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BetSafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
