28d337875823ffb35e50fd706982410f759aaed3
kistegez <tegeza@gmail.com>
2022-03-24 16:41:59 +0100

    Feature - testing the connection file

--- BetSafe/src/main/java/betsafe/dao/MatchDaoCsv.java
+++ BetSafe/src/main/java/betsafe/dao/MatchDaoCsv.java
@@ -13,8 +13,9 @@ public class MatchDaoCsv {
     public List<MatchModel> convertFiles(List<String> files) throws IOException {
         List<MatchModel> matches = new ArrayList<>();
         String line = "";
+        String mapping = "src/main/resources/csvData/";
         for (String path : files) {
-            BufferedReader br = new BufferedReader(new java.io.FileReader(path));
+            BufferedReader br = new BufferedReader(new java.io.FileReader(mapping + path));
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
                 MatchModel matchModel = new MatchModel(values[1], values[2] + " ," + values[3], values[4], values[5]);
