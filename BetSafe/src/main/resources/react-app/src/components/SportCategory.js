a6efde3eeba95bf93fb9896aaabdd84e14c2794d
enderjano <ender.janos01@gmail.com>
2022-04-06 15:03:17 +0200

    Feature - basic frontend look

--- BetSafe/src/main/resources/react-app/src/components/SportCategory.js
+++ BetSafe/src/main/resources/react-app/src/components/SportCategory.js
@@ -1,9 +1,7 @@
 const SportCategory = (props) => {
     return (
         <div className="left-nav">
-            <a href="#">Tennis</a>
-            <br></br>
-
+            <a href="#">Tennis</a><br></br>
             <a href="#">Ping-Pong</a><br></br>
             <a href="#">E-sport</a><br></br>
         </div>
