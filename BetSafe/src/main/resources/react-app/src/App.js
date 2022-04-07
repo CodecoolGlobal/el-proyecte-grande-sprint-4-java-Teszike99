6230aecdc4dfd3ce2ae81335e5ee732a171c03fb
Tesszike99 <teszidani02@gmail.com>
2022-04-07 15:22:29 +0200

    Featrue - hook, click logic

--- BetSafe/src/main/resources/react-app/src/App.js
+++ BetSafe/src/main/resources/react-app/src/App.js
@@ -2,15 +2,26 @@ import Header from './components/Header';
 import Body from './components/Body'
 import Navbar from './components/Navbar'
 import './App.css';
+import HomePage from "./components/HomePage";
+import Matches from "./components/Matches";
+import {useState} from "react";
 
 function App() {
-
-
+    const components = [<HomePage/>, <Matches/>] // Need this to change body contex
+    const [componentIndex, setIndex] = useState(0); //use state hook
+    function handleClick(page){
+        if(page === "matches"){
+            setIndex(1)
+        }
+        else if(page === "home"){
+            setIndex(0)
+        }
+    }
   return (
     <div className="App">
       <Header/>
-        <Navbar/>
-      <Body />
+        <Navbar component={components[componentIndex]} onClick={handleClick}/>
+        <Body component={components[componentIndex]}/>
     </div>
   );
 }
