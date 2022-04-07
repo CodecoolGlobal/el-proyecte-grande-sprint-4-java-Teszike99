07966b568b8f61f331b30298cc84721242bd3c8d
Tesszike99 <teszidani02@gmail.com>
2022-04-07 14:33:02 +0200

    Fix - remove content

--- BetSafe/src/main/resources/react-app/src/components/BodyContent.js
+++ BetSafe/src/main/resources/react-app/src/components/BodyContent.js
@@ -2,17 +2,7 @@ import image from "../images/bet.png";
 
 const BodyContent = (props) => {
     return (
-        <div className="main-container">
-            <h1>Live your life, free from risk</h1><br></br>
-            <img className="image-container" src={image} alt=''/>
-            <br/><br/><br/>
-            <b>
-                BetSafe is an online betting company that offers online sports betting.
-                On this site you can place sports bets with the least possible risk.
-                You can enter a sum of money after which the site calculates the best options
-                based on match odds and betting offices.
-            </b>
-        </div>
+      null
     )
 }
 
