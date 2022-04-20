f22876aa4ff875fec169679116fb22d5ade45d07
kistegez <tegeza@gmail.com>
2022-04-20 22:58:39 +0200

    Feature - BettingOffice repository created

---
+++ BetSafe/src/main/java/betsafe/repository/BettingOfficeRepository.java
@@ -0,0 +1,8 @@
+package betsafe.repository;
+
+import betsafe.model.BettingOffice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BettingOfficeRepository extends JpaRepository<BettingOffice, Long> {
+
+}
