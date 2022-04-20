af6c69656188a8eede4f3b4e9c11fdeeee04fcac
kistegez <tegeza@gmail.com>
2022-04-20 22:32:34 +0200

    Feature - filtering by sport and bettingOffice in Repository

--- BetSafe/src/main/java/betsafe/repository/MatchRepository.java
+++ BetSafe/src/main/java/betsafe/repository/MatchRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MatchRepository extends JpaRepository <Match, Long> {
+
+    List<Match> findMatchesByBettingOffice(String office);
+
+    List<Match> findMatchesBySportType(String sport);
 }
