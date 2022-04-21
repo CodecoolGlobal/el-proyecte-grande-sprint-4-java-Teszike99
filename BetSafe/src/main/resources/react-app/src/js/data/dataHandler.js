e423b33b33d88c01a0521c28f1a1bc369973e8a1
Tesszike99 <teszidani02@gmail.com>
2022-04-21 11:59:59 +0200

    Feature - post method frontend

--- BetSafe/src/main/resources/react-app/src/js/data/dataHandler.js
+++ BetSafe/src/main/resources/react-app/src/js/data/dataHandler.js
@@ -7,3 +7,16 @@ export async function apiGet(url) {
             return data;
         }
 }
+
+export async function apiPost(url, payload) {
+    let response = await fetch(url, {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(payload)
+    });
+    if (response.status === 200) {
+        let data = response.json();
+        return data;
+
+    }
+}
