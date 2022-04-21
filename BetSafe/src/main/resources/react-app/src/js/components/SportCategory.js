71d759a6ccccbde0bbe3f33b69b3c7b95162d2f4
Tesszike99 <teszidani02@gmail.com>
2022-04-21 10:13:07 +0200

    Feature - idk

--- BetSafe/src/main/resources/react-app/src/js/components/SportCategory.js
+++ BetSafe/src/main/resources/react-app/src/js/components/SportCategory.js
@@ -1,6 +1,6 @@
 const SportCategory = (props) => {
     return (
-        <div className="left-nav">
+        <div className="left-nav col-md">
             <a href="#">Tennis</a><br></br>
             <a href="#">Ping-Pong</a><br></br>
             <a href="#">E-sport</a><br></br>
