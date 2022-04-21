54010f6558fd7c20abe156776e59a3c7c73443ee
Tesszike99 <teszidani02@gmail.com>
2022-04-21 11:38:21 +0200

    Fix - profit calculation invest page

--- BetSafe/src/main/resources/react-app/src/js/components/MatchPair.js
+++ BetSafe/src/main/resources/react-app/src/js/components/MatchPair.js
@@ -3,6 +3,7 @@ const MatchPair = (props) =>  {
     let secondMatch = props.matchPairData[1];
     let investHome = parseFloat(props.investHome);
     let investGuest = parseFloat(props.investGuest);
+    let sumOfInvest = investHome + investGuest;
     // props.invest (remembering)
     return (
         <div className= "match-pair-container row">
@@ -13,7 +14,7 @@ const MatchPair = (props) =>  {
                     <div className="odds-container">
                         <h3><font color = "rgb(255,255,255)">Home odds: </font><font color= "#ff4500"><b>{firstMatch.homeOdds}</b></font></h3>
                         <h3>Guest odds: <font color= "#ff4500"><b>{firstMatch.guestOdds}</b></font></h3>
-                        <h3>Profit: {investHome * parseFloat(firstMatch.homeOdds)}</h3>
+                        <h3>Profit: {sumOfInvest - (investHome * parseFloat(firstMatch.homeOdds))}</h3>
                     </div>
                 </div>
                 <div className="match-container second-match col-sm-6">
@@ -21,7 +22,7 @@ const MatchPair = (props) =>  {
                     <div className="odds-container">
                         <h3>Home odds: <font color= "#ff4500"><b>{secondMatch.homeOdds}</b></font></h3>
                         <h3><font color = "rgb(255,255,255)">Guest odds: </font><font color= "#ff4500"><b>{secondMatch.guestOdds}</b></font></h3>
-                        <h3>Profit: {investGuest * parseFloat(secondMatch.guestOdds)}</h3>
+                        <h3>Profit: {sumOfInvest - (investGuest * parseFloat(secondMatch.guestOdds))}</h3>
                     </div>
                 </div>
             </div>
