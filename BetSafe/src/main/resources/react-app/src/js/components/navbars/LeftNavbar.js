760f3cc84e28a32b461bca483c4e427df2c16863
Tesszike99 <teszidani02@gmail.com>
2022-05-02 15:37:06 +0200

    Fix - change filter arg names

--- BetSafe/src/main/resources/react-app/src/js/components/navbars/LeftNavbar.js
+++ BetSafe/src/main/resources/react-app/src/js/components/navbars/LeftNavbar.js
@@ -2,8 +2,8 @@ const LeftNavbar = (props) => {
     return (
         <div className="left-nav col-md">
             <div onClick={event => props.setFilter("tennis")}>Tennis</div>
-            <div onClick={event => props.setFilter("e-sport")}>E-sport</div>
-            <div onClick={event => props.setFilter("ping-pong")}>PinPong</div>
+            <div onClick={event => props.setFilter("E-sport")}>E-sport</div>
+            <div onClick={event => props.setFilter("Ping-Pong")}>PinPong</div>
         </div>
     )
 }
