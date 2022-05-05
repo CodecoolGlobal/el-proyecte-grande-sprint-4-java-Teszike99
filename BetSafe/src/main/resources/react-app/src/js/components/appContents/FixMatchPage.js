1bc197d99f98bbc35f727bc7616fdcd09d39db57
Tesszike99 <teszidani02@gmail.com>
2022-05-05 14:04:51 +0200

    Featrue - filter for fixMatch page

--- BetSafe/src/main/resources/react-app/src/js/components/appContents/FixMatchPage.js
+++ BetSafe/src/main/resources/react-app/src/js/components/appContents/FixMatchPage.js
@@ -5,11 +5,28 @@ import MatchPair from "../contentTools/MatchPair";
 const FixMatchPage = (props) => {
     const [data, setData] = useState([])
     const matchPairList = data.map( matchPair => <MatchPair matchPairData={matchPair}/> );
+    //list created to check filter type (sport or betting office)
+    let sportCategories = ["tennis", "E-sport", "Ping-Pong"];
+    let officeCategories = ["BetterBet", "Esport", "MegaGame", "UniBet"];
+    let filter = props.filter;
+
     useEffect(() => {
-        apiGet("fix-match-pairs")
-            .then(r => {setData(r)});
-    },[])
-    console.log(props.filter)
+        if (filter != null){
+            if (sportCategories.includes(filter)){
+                apiGet(`search-sport/${filter}`)
+                    .then(r => setData(r))
+            }
+            else if (officeCategories.includes(filter)){
+                apiGet(`search-office/${filter}`)
+                    .then(r => setData(r))
+            }
+        }
+        else {
+            apiGet("/fix-match-pairs")
+                .then(r => {setData(r)});
+        }
+    },[filter])
+
     return (
         <div className="fix-match-page-container">
             {matchPairList}
