514821963c3a54f7bccefa30abda43fbf38c3f82
kistegez <tegeza@gmail.com>
2022-05-19 22:55:41 +0200

    Fix - beginning state is 0 for invest

--- BetSafe/src/main/resources/react-app/src/js/components/appContents/InvestPage.js
+++ BetSafe/src/main/resources/react-app/src/js/components/appContents/InvestPage.js
@@ -3,8 +3,8 @@ import {apiGet} from "../../data/dataHandler";
 import MatchPair from "../contentTools/MatchPair";
 const InvestPage = (props) => {
     const [data, setData] = useState([])
-    const [investHomeText, setInvestHomeText] = useState(null);
-    const [investGuestText, setInvestGuestText] = useState(null);
+    const [investHomeText, setInvestHomeText] = useState(0);
+    const [investGuestText, setInvestGuestText] = useState(0);
     const matchPairList = data.map( matchPair => <MatchPair matchPairData={matchPair} investHome={investHomeText} investGuest={investGuestText} profit={true}/> );
     //list created to check filter type (sport or betting office)
     let sportCategories = ["tennis", "E-sport", "Ping-Pong"];
@@ -38,19 +38,16 @@ const InvestPage = (props) => {
 
     console.log(data)
     return (
+
         <div className="invest-page-container">
-            <div className="invest-container-title row">
-                <h3 className="col-sm-6">Best home odds</h3>
-                <h3 className="col-sm-6">Best guest odds</h3>
-            </div>
             <div className="invest-calculate-container row">
                 <div className="col-sm-6">
-                    <label htmlFor="invest-home-odds">Invest home odds:</label>
-                    <font color = "black"><input type="text" id="invest-home-odds" onChange={getHomeInvestData}/></font>
+                    <label htmlFor="invest-home-odds" >Invest home odds:</label>
+                    <font color = "black"><input type="number" id="invest-home-odds" defaultValue={0} onChange={getHomeInvestData}/></font>
                 </div>
                 <div className="col-sm-6">
                     <label htmlFor="invest-guest-odds">Invest guest odds:</label>
-                    <font color = "black"><input type="text" id="invest-guest-odds" onChange={getGuestInvestData}/></font>
+                    <font color = "black"><input type="number" id="invest-guest-odds" defaultValue={0} onChange={getGuestInvestData}/></font>
                 </div>
             </div>
             {matchPairList}
