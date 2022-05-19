efa76fafb8c4c74ca2e5fba67959078dc3901801
kistegez <tegeza@gmail.com>
2022-05-19 23:58:39 +0200

    Style - logo and name in the header

--- BetSafe/src/main/resources/react-app/src/js/components/navbars/Header.js
+++ BetSafe/src/main/resources/react-app/src/js/components/navbars/Header.js
@@ -1,25 +1,24 @@
-const Header = (props) => {
-    return (
-        <div className="header">
-            <h1 className="header-text">BetSafe</h1>
-            {props.currentUser ?
-                <p>
-                    {props.currentUser}
-                </p>
-                :
-                <p>
-                    <a href="/authenticate">Login</a>
-                </p>}
-            {props.currentUser ?
-                <p><a onClick={() => {
-                    props.logOut();
-                }}>Log out</a></p>
-                :
-                <p>
-                    <a href="/register">Register</a>
-                </p>
-            }
-        </div>
+import Button from '../contentTools/Button';
+import {Link} from "react-router-dom";
+import image from "../../../images/bat.png";
+import batmanImage from "../../../images/batman.png";
+import batsImage from "../../../images/bats.png";
+
+const loginButtonTitle = "Login"
+const loginButtonClassName = "login-button"
+const registerButtonTitle = "Create account"
+const registerButtonClassName = "register-button"
+
+ const Header = (props) => {
+     return (
+         <div className="header">
+
+             <img className="image-header" id="bats1" src={batsImage} alt=''/>
+             <img className="image-header" id="batman" src={batmanImage} alt=''/>
+             <img className="image-header" id="bat_title" src={image} alt=''/>
+             <img className="image-header" id="bats2" src={batsImage} alt=''/>
+         </div>
+
     )
 }
 
