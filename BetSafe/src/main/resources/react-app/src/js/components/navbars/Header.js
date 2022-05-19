79220efee7d64299f51c44a8666715363bd2c018
kistegez <tegeza@gmail.com>
2022-05-19 22:54:39 +0200

    Style - header with logo

--- BetSafe/src/main/resources/react-app/src/js/components/navbars/Header.js
+++ BetSafe/src/main/resources/react-app/src/js/components/navbars/Header.js
@@ -1,5 +1,8 @@
 import Button from '../contentTools/Button';
 import {Link} from "react-router-dom";
+import image from "../../../images/bat.png";
+import batmanImage from "../../../images/batman.png";
+import batsImage from "../../../images/bats.png";
 
 const loginButtonTitle = "Login"
 const loginButtonClassName = "login-button"
@@ -9,9 +12,12 @@ const registerButtonClassName = "register-button"
 const Header = (props) => {
     return (
         <div className="header">
-            <h1 className="header-text">BetSafe</h1>
-            <a href="/register">Reg</a>
-            <a href="/login">Log</a>
+
+            <img className="image-header" id="bats1" src={batsImage} alt=''/>
+            <img className="image-header" id="batman" src={batmanImage} alt=''/>
+            <img className="image-header" id="bat_title" src={image} alt=''/>
+            <img className="image-header" id="bats2" src={batsImage} alt=''/>
+
         </div>
     )
 }
